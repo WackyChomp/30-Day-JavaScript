bc0adcff0259230b2d7aab94e602758a33d32d7b
Wilson Chan <wilsonchan54@gmail.com>
2020-10-16 00:33:49 -0400

    Have cursor follow the mouse when scrolling (not working properly)

--- 1 - Custom Cursor/app.js
+++ 1 - Custom Cursor/app.js
@@ -4,5 +4,13 @@ const cursor = document.querySelector('.cursor');
 window.addEventListener('mousemove', (e)=>{
     cursor.style.left = e.pageX + 'px';        /*Adding the 'px' causes the cursor to follow the mouse */
     cursor.style.top = e.pageY + 'px';
-    console.log(e.pageX, e.pageY)    /*Shows the coordinates*/
+    /*console.log(e.pageX, e.pageY)    Shows the coordinates*/
+    cursor.setAttribute('data-fromTop', (cursor.offsetTop - scrollY))
 });
+
+/*Cursor following the mouse */
+window.addEventListener('scroll', ()=>{
+    const fromTop = parseInt(cursor.getAttribute('data-fromTop'));     /*takes value from setAttribute above*/
+    cursor.style.top = scrollY + 'px';
+    /*console.log(scrollY);         see the value of scroll  */
+})
