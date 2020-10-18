eb129b57c9c58fc107580394264d192109551b90
Wilson Chan <wilsonchan54@gmail.com>
2020-10-18 15:12:33 -0400

    Two methods of replacing default text in html with fetched joke from API

--- 3 - Joke Generator/app.js
+++ 3 - Joke Generator/app.js
@@ -1,8 +1,20 @@
 const button = document.querySelector('.container button');
 const jokeText = document.querySelector('.container p');
+document.addEventListener('DOMContentLoaded', getJoke);   /*display joke instead of default text from HTML*/
 
 button.addEventListener('click', getJoke);
 
+//Method 1
+function getJoke(){
+    fetch('https://icanhazdadjoke.com/', {
+        headers:{
+            'Accept' : 'application/json'
+        }
+    }).then(data => data.json())
+        .then(object => jokeText.innerHTML = object.joke);
+}
+
+/*    //Alternative Method: using async and await to fetch data
 async function getJoke(){
     const jokeData = await fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -11,5 +23,6 @@ async function getJoke(){
     });
     const jokeObject = await jokeData.json();
     jokeText.innerHTML = jokeObject.joke;        /*the joke being fetch replaces default text in index.html*/
-    /*console.log(jokeObject.joke)       /*the joke data is being fetch and displayed in console log*/
-}
+    /*console.log(jokeObject.joke)*/       /*the joke data is being fetch and displayed in console log*/
+    //};
+
