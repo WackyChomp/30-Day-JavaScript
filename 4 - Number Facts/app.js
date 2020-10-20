d8889c272a521b385e511e079af0f832ee3e37b8
Wilson Chan <wilsonchan54@gmail.com>
2020-10-20 00:51:15 -0400

    Using cors-anywhere to make the numbersapi more secure

--- 4 - Number Facts/app.js
+++ 4 - Number Facts/app.js
@@ -6,9 +6,18 @@ form.addEventListener('submit', (e)=>{
     const number = e.target.querySelector('input[type = "number"]').value;
     const loadText = "Wait for a moment...";
     factDiv.innerHTML = loadText;       //shows loadText when API fetches the data
-    const numURL = "http://numbersapi.com/";       //https://cors-anywhere.herokuapp.com/
-    fetch(numURL + number)
+    const numURL = "https://cors-anywhere.herokuapp.com/http://numbersapi.com/";
+    fetch(numURL + number, {
+        method: "GET",
+        headers: {
+            'x-requested-with': 'text/plain'
+        }})
         .then(response => response.text())      /*console.log(response) */
         .then(text => factDiv.innerHTML = text);      /*console.log(text) */
     //console.log(number);    //shows number input in console
 })
+
+/*
+Using   https://cors-anywhere.herokuapp.com/  it doesn't work in html "open with live server".
+Removing this URL extension works but it will not be secure
+*/
