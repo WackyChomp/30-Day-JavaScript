f53be33d44b13efd4c5c33cb00dc6c2e964a5b41
Wilson Chan <wilsonchan54@gmail.com>
2020-10-21 00:15:23 -0400

    Timer counting down shows decimals

--- 5 - Countdown Timer/app.js
+++ 5 - Countdown Timer/app.js
@@ -5,5 +5,14 @@ timeHeader.innerHTML = `00:${timeSecond}`;    //not ' ' it's ` `
 
 const countDown = setInterval ( ()=> {
     timeSecond--;
-    timeHeader.innerHTML = `00:${timeSecond}`;
+    displayTime(timeSecond);
+    if(timeSecond < 0 || timeSecond < 1){        //prevents going beyond zero into negative
+        clearInterval(countDown)
+    }
 }, 1000)
+
+function displayTime(second){
+    const min = second / 60;
+    const sec = second % 60;
+    timeHeader.innerHTML = `${min}:${sec}`
+}
