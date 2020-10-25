39d4809159195ed342b56cc0c140ec70aaaf1463
Wilson Chan <wilsonchan54@gmail.com>
2020-10-25 19:14:00 -0400

    Having the word 'space' appear when pressed / creating the copy functionality when clicking the value under 'Key Code'

--- 7 - Key Code Generator/app.js
+++ 7 - Key Code Generator/app.js
@@ -2,7 +2,25 @@ const displayKey = document.querySelector('.key h2');
 const displayKeyCode = document.querySelector('.keyCode h2');
 const keyCodeDiv = document.querySelector('.keyCode');
 
-window.addEventListener('keypress' , (e)=> {
+window.addEventListener('keypress' , (e)=>{
     displayKey.innerText = e.key;          /*display what key is being pressed*/
     displayKeyCode.innerText = e.keyCode;     /*keyCode is depricated*/
+    if(e.keyCode === 32){                  /*makes the word "space" appear after pressing it*/
+        displayKey.innerText = `"space"`;
+    };
+})
+
+keyCodeDiv.addEventListener('click' , (e)=>{
+    const textArea = document.createElement('textarea');
+    textArea.setAttribute('readonly', '');      /*allows us to see it */
+    textArea.style.position = 'absolute';
+    textArea.value = keyCodeDiv.querySelector('h2').innerText;
+    document.body.appendChild(textArea)
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea)       /*prevents text area from appearing*/
+    keyCodeDiv.querySelector('p').innerText = 'Copied'     /*output says copied when clicked*/
+    setTimeout(()=>{        /*reverts back to original text from 'Copied'*/
+        keyCodeDiv.querySelector('p').innerText = 'Click to Copy'
+    }, 2000)
 })
