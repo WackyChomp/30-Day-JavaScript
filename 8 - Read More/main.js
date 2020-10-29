3a98aa279d2987807a4282d2a0ff48af175c2c4e
Wilson Chan <wilsonchan54@gmail.com>
2020-10-28 23:54:43 -0400

    Pressing button alternates between 'Read More...' and 'Read Less' / Add trailing dots to insinuate more text ahead

--- 8 - Read More/main.js
+++ 8 - Read More/main.js
@@ -3,4 +3,10 @@ const text = document.querySelector('.text');
 
 readMoreButton.addEventListener('click' , (e)=>{
     text.classList.toggle('show-more');
+    if(readMoreButton.innerText === 'Read More...'){
+        readMoreButton.innerText = "Read Less";
+    }
+    else{
+        readMoreButton.innerText = "Read More...";
+    }
 })
