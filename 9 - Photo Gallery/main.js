f4136a915e9c538298d49d32f75bf00d1d874e3c
Wilson Chan <wilsonchan54@gmail.com>
2020-10-31 17:57:18 -0400

    Creating class PhotoGallery that grabs images from Pexel API

--- 9 - Photo Gallery/main.js
+++ 9 - Photo Gallery/main.js
@@ -1,2 +1,31 @@
-const API_KEY = `THE API KEY GOES HERE`;
 
+/*Object-oriented approach*/
+class PhotoGallery{
+    constructor(){
+        this.API_KEY = `THE API KEY GOES HERE`;
+        this.galleryDiv = document.querySelector('.gallery');
+        this.searchForm = document.querySelector('.header form');
+        this.loadMore = document.querySelector('.load-more');
+        this.eventHandle();
+    }
+
+    eventHandle(){        /*when DOM content loads after the load it will grab images from API */
+        document.addEventListener('DOMContentLoaded', ()=>{
+            this.getImg();
+        });
+    }
+
+    async getImg(){
+        const baseURL = "https://api.pexels.com/v1/search?query=nature&per_page=1";
+        const response = await fetch(baseURL, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                Authorization: this.API_KEY
+            }
+        })
+        console.log(response);
+    }
+}
+
+const gallery = new PhotoGallery;    /*initalize and call this class */
