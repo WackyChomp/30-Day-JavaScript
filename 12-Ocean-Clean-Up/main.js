4c301b621aa080ad155ca2a245ed2c17e2a2bc26
Wilson Chan <wilsonchan54@gmail.com>
2021-12-25 23:46:15 -0500

    Created async function setupDebris to fetch live donation data / created variables trashFormatter (fill whitespace with 0's) and remainingDonation (funds required to meet target goal)

--- 12-Ocean-Clean-Up/main.js
+++ 12-Ocean-Clean-Up/main.js
@@ -12,7 +12,38 @@ const currencyFormatter = new Intl.NumberFormat("en-us", {
     currency: "USD",
     maximumFractionDigits: 0
 })
-const amountCollected = 3000192
-donationElement.innerText = currencyFormatter.format(amountCollected)
+
+/* Pad numbers with 0 to avoid whitespace  */
+const trashFormatter = new Intl.NumberFormat("en-us", {
+    minimumIntegerDigits: 8,
+    maximumFractionDigits: 0,
+    useGrouping: false
+})
+console.log(trashFormatter.format(722))
+
+
+const DONATION_TARGET = 30000000
+
+
+
+// const amountCollected = 3000192
+// donationElement.innerText = currencyFormatter.format(amountCollected)
+setupDebris()       /* call async function below */
 
 /* -------------------------------------------- */
+
+/*  */
+
+async function setupDebris(){
+    const amountCollected = await fetch("https://tscache.com/donation_total.json")
+    .then(res => res.json())
+    .then(data => data.count)
+    donationElement.innerText = currencyFormatter.format(amountCollected)
+
+
+    /* Calculation for donation goal */
+    const remainingDonation = Math.max(DONATION_TARGET - amountCollected, 0)     /* ensure no negative numbers past 0 */
+
+    
+}
+
