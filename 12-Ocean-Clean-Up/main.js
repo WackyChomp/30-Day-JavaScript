8a21e0e4205d904f334c12b5cc9cf3492e0af9bc
Wilson Chan <wilsonchan54@gmail.com>
2022-01-10 23:08:04 -0500

    Added keyframes to bobbing animation in style.css / using css rotation with functionrandomNumberBetween in main.js

--- 12-Ocean-Clean-Up/main.js
+++ 12-Ocean-Clean-Up/main.js
@@ -87,13 +87,16 @@ function createDebris(icon){
     const img = document.createElement('img')
     const top = randomNumberBetween(0, 50)
     const size = top / 5 + 1
-    img.src = `/imgs/${icon}.svg`
+    img.classList.add("debris")
+    img.src = `./imgs/${icon}.svg`
     img.style.top = `${top}vh`
     img.style.width = `${size}vmin`
     img.style.height = `${size}vmin`
-    img.style.left = `${randomNumberBetween(0, 100)}vw`
+    img.style.left = `${randomNumberBetween(0, 100)}vw`   /* spans horizontally across screen */
+    img.style.setProperty("--rotation", `${randomNumberBetween(-30, 30)}deg`)
     debrisContainer.appendChild(img)
 }
+
 
 function randomNumberBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
